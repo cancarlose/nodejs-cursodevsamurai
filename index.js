f9d5b064c13fa6fdbd5e1c5b3e00e62afa04b474
canCarlose <91757480+cancarlose@users.noreply.github.com>
2023-10-17 18:24:28 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-// requerindo o express
 const express = require("express");
-// criando o servidor
 const server = express();
 
-// criando uma rota
+// Criando a Rota
 server.get("/hello", (req, res) => {
-    return res.send("Hello World"); 
+  return res.send("Hello World")
 });
 
 
-// localhost, endereço especial 127.0.0.1
-server.listen(3000);
-
-// portas comuns, 3000, 4000, 5000, 8000
+// Local Host 127.0.0.1
+server.listen(5173);
+// Portas comuns, 3000, 4000, 5000, 8000
