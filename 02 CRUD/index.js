f683425677a2922d3d9cbf375339e1911f2a50d0
cancarlose <carlosedlima4@gmail.com>
2023-10-21 20:14:55 -0300

    commit 21/10

--- 02 CRUD/index.js
+++ 02 CRUD/index.js
@@ -1,20 +1,25 @@
 // Criando e Manipulando CRUD Básico
+
+// Criando servidor ExpressJS
 const express = require("express");
 const server = express();
 
 server.use(express.json());
 
+// Array Customers
 let customers = [
     { id: 1, name: "Facebook", site: "http://facebook.com"},
     { id: 2, name: "Google", site: "http://google.com"},
     { id: 3, name: "UOL", site: "http://uol.com.br"}
 ];
 
+// Método de captura dos customers
 server.get("/customers", (req, res) => {
     return res.json(customers);
 });
 
 
+// Método de captura de forma mais especifica
 server.get("/customers/:id", (req, res) => {
     const id = parseInt(req.params.id);
     const customer = customers.find(item => item.id === id); 
@@ -23,6 +28,8 @@ server.get("/customers/:id", (req, res) => {
     return res.status(status).json(customer);
 });
 
+
+// Criação de novo custormer
 server.post("/customers", (req, res) => {
     const { name, site } = req.body;
     const id = customers[customers.length - 1].id + 1;
@@ -33,6 +40,8 @@ server.post("/customers", (req, res) => {
     return res.status(201).json(newCustomer);
 });
 
+
+// Método de atualização de customers
 server.put("/customers/:id", (req, res) => {
     const id = parseInt(req.params.id);
     const { name, site } = req.body;
@@ -45,6 +54,20 @@ server.put("/customers/:id", (req, res) => {
     };
 
     return res.status(status).json(customers[index]);
+});
+
+
+// Método de exclusão de customer
+server.delete("/customers/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = customers.findIndex(item => item.id === id);
+    const status = index >= 0 ? 200 : 404;
+
+    if (index >= 0) {
+        customers.splice(index, 1);
+    }
+
+    return res.status(status).json();
 });
 
 server.listen(3173);
