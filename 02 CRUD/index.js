4ab7b91b25390570928d37db7683d8ae357161bf
cancarlose <carlosedlima4@gmail.com>
2023-10-20 23:33:13 -0300

    commit 20/10

--- 02 CRUD/index.js
+++ 02 CRUD/index.js
@@ -17,7 +17,7 @@ server.get("/customers", (req, res) => {
 
 server.get("/customers/:id", (req, res) => {
     const id = parseInt(req.params.id);
-    const customer = customers.find(item => item.id === id);
+    const customer = customers.find(item => item.id === id); 
     const status = customer ? 200 : 404;
 
     return res.status(status).json(customer);
