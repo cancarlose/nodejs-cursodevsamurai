0888da98308eb342c0d5e7686d477469356ccc38
cancarlose <carlosedlima4@gmail.com>
2023-10-26 15:00:23 -0300

    commit

--- src/app.js
+++ src/app.js
@@ -1,28 +1,28 @@
 // Requerimento do ExpressJS
-import express from "express";
+import express from 'express';
 // trazendo as rotas
-import routes from "./routes.js";
+import routes from './routes.js';
 // Parte de importação acima ^
 
 class App {
-    constructor() {
-        // criação do servidor expressJS
-        this.server = express();
-        // invocação dos métodos
-        this.middlewares();
-        this.routes();
-    }
-    middlewares() {
-        // midleware que se permite trabalhar com json
-        this.server.use(express.json());
-    }
+  constructor() {
+    // criação do servidor expressJS
+    this.server = express();
+    // invocação dos métodos
+    this.middlewares();
+    this.routes();
+  }
 
-    routes() {
-        // trazendo o arquivo das rotas
-        this.server.use(routes);
-    }
-}; // Classe onde vai conter nossa lógica.
+  middlewares() {
+    // midleware que se permite trabalhar com json
+    this.server.use(express.json());
+  }
 
+  routes() {
+    // trazendo o arquivo das rotas
+    this.server.use(routes);
+  }
+} // Classe onde vai conter nossa lógica.
 
 // sempre que importar algo com require, tudo no export sera retornado.
 export default new App().server;// Dessa forma, exportamos esse módulo.
--- src/app/controller/CustomersController.js
+++ src/app/controller/CustomersController.js
@@ -1,64 +1,64 @@
-let customers = [
-    { id: 1, name: "Facebook", site: "http://facebook.com"},
-    { id: 2, name: "Google", site: "http://google.com"},
-    { id: 3, name: "UOL", site: "http://uol.com.br"}
+const customers = [
+  { id: 1, name: 'Facebook', site: 'http://facebook.com' },
+  { id: 2, name: 'Google', site: 'http://google.com' },
+  { id: 3, name: 'UOL', site: 'http://uol.com.br' },
 ];
 
 class CustomersController {
-    // Listagem dos Customers
-    index(req, res) {
-        return res.json(customers);
+  // Listagem dos Customers
+  index(req, res) {
+    return res.json(customers);
+  }
+
+  // Recupera um Customer
+  show(req, res) {
+    const id = parseInt(req.params.id);
+    const status = customers ? 200 : 404;
+    const customer = customers.find((item) => item.id === id);
+
+    console.log('GET :: /customers/id ", JSON.stringify(customer)');
+
+    return res.status(status).json(customer);
+  }
+
+  // Cria um novo Customer
+  create(req, res) {
+    const { name, site } = req.body;
+
+    const id = customers[customers.length - 1].id + 1;
+    const newCustomer = { id, name, site };
+    customers.push(newCustomer);
+
+    return res.status(201).json(newCustomer);
+  }
+
+  // Atualiza um Customer
+  update(req, res) {
+    const id = parseInt(req.params.id);
+    const { name, site } = req.body;
+
+    const index = customers.findIndex((item) => item.id === id);
+    const status = index >= 0 ? 200 : 404;
+
+    if (index >= 0) {
+      customers[index] = { id: parseInt(id), name, site };
     }
 
-    // Recupera um Customer
-    show(req, res) {
-        const id = parseInt(req.params.id);
-        const status = customers ? 200 : 404;
-        const customer = customers.find(item => item.id === id); 
+    return res.status(status).json(customers[index]);
+  }
 
-        console.log("GET :: /customers/id ", JSON.stringify(customer));
+  // Exclui um Customer
+  destroy(req, res) {
+    const id = parseInt(req.params.id);
+    const index = customers.findIndex((item) => item.id === id);
+    const status = index >= 0 ? 200 : 404;
 
-        return res.status(status).json(customer);
+    if (index >= 0) {
+      customers.splice(index, 1);
     }
 
-    // Cria um novo Customer
-    create(req, res) {
-        const { name, site } = req.body;
-        
-        const id = customers[customers.length - 1].id + 1;
-        const newCustomer = { id, name, site };
-        customers.push(newCustomer);
-
-        return res.status(201).json(newCustomer);
-    }
-
-    // Atualiza um Customer
-    update(req, res) {
-        const id = parseInt(req.params.id);
-        const { name, site } = req.body;
-
-        const index = customers.findIndex(item => item.id === id);
-        const status = index >= 0 ? 200 : 404;
-
-        if (index >= 0) {
-        customers[index] = { id: parseInt(id), name, site };
-        };
-
-        return res.status(status).json(customers[index]);
-    }
-
-    // Exclui um Customer
-    destroy(req, res) {
-        const id = parseInt(req.params.id);
-        const index = customers.findIndex(item => item.id === id);
-        const status = index >= 0 ? 200 : 404;
-
-        if (index >= 0) {
-        customers.splice(index, 1);
-        }
-
-        return res.status(status).json();
-    }
-};
+    return res.status(status).json();
+  }
+}
 
 export default new CustomersController();
