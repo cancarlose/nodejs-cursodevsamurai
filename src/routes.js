f43fe5cdef49569ab343cbd20c459288af220d0a
cancarlose <carlosedlima4@gmail.com>
2023-10-26 11:37:50 -0300

    commit 26/10

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,14 @@
 const { Router } = require("express"); // sistema de rotas do express.
 const routes = new Router(); // possibilidade de ter as rotas
 
-routes.get("/hello", (req, res) => {
-    return res.json({ message: "Hello World"});
-});
+// Requirindo o controller
+const customers = require("./app/controller/CustomersController");
+
+// Agora, controller responsável pelos métodos
+routes.get("/customers", customers.index);
+routes.get("/customers/:id", customers.show);
+routes.post("/customers", customers.create);
+routes.put("/customers/:id", customers.update);
+routes.delete("/customers/:id", customers.destroy);
 
 module.exports = routes; // exportação
